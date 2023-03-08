5d0dbd5ed78b7096ec75d79559ef3992f0fdee99
h-suo <phstoo1021@naver.com>
2023-03-08 16:34:03 +0900

    Commit MARK

--- Rx_Test_Project/ViewController.swift
+++ Rx_Test_Project/ViewController.swift
@@ -22,62 +22,52 @@ class ViewController: UIViewController {
         setupRx()
     }
     
-    func setupBinding() {
-        textField.rx.text.orEmpty
-            .bind(to: lable.rx.text)
-            .disposed(by: disposeBag)
-        
-        textField.rx.text.orEmpty
-            .map({ s -> Bool in
-                s == "" ? false : true
-            })
-            .observe(on: MainScheduler.instance)
-            .bind(to: clearButton.rx.isEnabled)
-            .disposed(by: disposeBag)
-        
-        clearButton.rx.tap
-            .do(onNext: {
-                self.clearButton.isEnabled = false
-            })
-            .map({ _ -> String in
-                return ""
-            })
-            .bind(to: textField.rx.text, lable.rx.text)
-            .disposed(by: disposeBag)
-    }
+    // MARK: - basic Rx Code
+//    func setupBinding() {
+//        textField.rx.text.orEmpty
+//            .bind(to: lable.rx.text)
+//            .disposed(by: disposeBag)
+//
+//        textField.rx.text.orEmpty
+//            .map({ s -> Bool in
+//                s == "" ? false : true
+//            })
+//            .observe(on: MainScheduler.instance)
+//            .bind(to: clearButton.rx.isEnabled)
+//            .disposed(by: disposeBag)
+//
+//        clearButton.rx.tap
+//            .do(onNext: {
+//                self.clearButton.isEnabled = false
+//            })
+//            .map({ _ -> String in
+//                return ""
+//            })
+//            .bind(to: textField.rx.text, lable.rx.text)
+//            .disposed(by: disposeBag)
+//    }
     
-    
+    // MARK: - input, output Rx Code
     func setupRx() {
                 
         // input
-        let textInputOb = textField.rx.text.orEmpty.asObservable()
-        let textEmptyCheckkOb = textInputOb.map({ s -> Bool in s == "" ? false : true })
-        let clearTapOb = clearButton.rx.tap.map({ _ -> String in return ""})
+        let textInputObservable = textField.rx.text.orEmpty.asObservable()
+        let textEmptyCheckkObservable = textInputObservable.map({ s -> Bool in s == "" ? false : true })
+        let clearTapObservable = clearButton.rx.tap.map({ _ -> String in return ""})
     
         // output
-        textInputOb
+        textInputObservable
             .bind(to: lable.rx.text)
             .disposed(by: disposeBag)
         
-        textEmptyCheckkOb
+        textEmptyCheckkObservable
             .bind(to: clearButton.rx.isEnabled)
             .disposed(by: disposeBag)
         
-        clearTapOb
+        clearTapObservable
             .do(onNext: { _ in self.clearButton.isEnabled = false })
             .bind(to: textField.rx.text, lable.rx.text)
             .disposed(by: disposeBag)
-        
-    //    idValidOb.subscribe(onNext: { b in self.idValidView.isHidden = b })
-    //        .disposed(by: disposeBag)
-    //    pwValidOb.subscribe(onNext: { b in self.pwValidView.isHidden = b })
-    //        .disposed(by: disposeBag)
-    //
-    //    Observable.combineLatest(idValidOb, pwValidOb, resultSelector: { $0 && $1 })
-    //        .subscribe(onNext: { b in
-    //            self.loginButton.isEnabled = b
-    //        })
-    //        .disposed(by: disposeBag)
     }
 }
 
